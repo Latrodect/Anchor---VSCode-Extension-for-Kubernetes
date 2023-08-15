3e0f35c75144e6ac8c86f51f03d968431d277ca7
Hasan Bahadır NURAL <h.bahadirnural@gmail.com>
2023-08-16 01:01:03 +0300

    new packed version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,13 +1,13 @@
 # Changelog
 
-## [1.3.0] Unreleased
+## [1.4.0] Unreleased
 - Docker Bash Repo Registry
 - Docker Bash Repo Push
 - Kubernetes Bash Deployment
 
-## [1.2.0] - 2023-08-08
+## [1.3.0] - 2023-08-08
 - Initial release
-- Autorun docker compose up.
-- Multiple Environment Variable Setup
+- Jobs generation added for k8s.
+- k8s Folder structure change.
 
-[1.2.0]: https://github.com/Latrodect/Backdoor-VSCode-Extension-for-K8S-Setup-and-Code-Review/releases/tag/v1.2.0
+[1.3.0]: https://github.com/Latrodect/Backdoor-VSCode-Extension-for-K8S-Setup-and-Code-Review/releases/tag/v1.2.0
--- out/commands/generateKubernetesFiles.js
+++ out/commands/generateKubernetesFiles.js
@@ -42,52 +42,90 @@ function generateKubernetesFiles() {
             vscode.window.showErrorMessage('No workspace is open.');
             return;
         }
-        const namespaceName = '';
+        // Namespace Input 
+        const namespaceInput = yield vscode.window.showInputBox({
+            prompt: 'Your Namespace:',
+            placeHolder: 'Example: my-application',
+        });
+        if (!namespaceInput) {
+            vscode.window.showWarningMessage('Please specify a namespace.');
+            return;
+        }
+        const namespace = checkSpacesAndReplace([namespaceInput]);
+        // Deployment Input 
         const deploymentsInput = yield vscode.window.showInputBox({
             prompt: 'Your Deployment Files:',
-            placeHolder: 'frontend, backend, redis',
+            placeHolder: 'Example: frontend, backend, redis ',
         });
         if (!deploymentsInput) {
-            vscode.window.showInformationMessage('Please specify atleast one deployment name.');
+            vscode.window.showWarningMessage('Please specify atleast one deployment name.');
             return;
         }
         const deploymentNames = checkSpacesAndReplace(deploymentsInput.split(',').map(name => name.trim()));
+        // Jobs Input 
+        const jobsInput = yield vscode.window.showInputBox({
+            prompt: 'Your Jobs:',
+            placeHolder: 'Example: collector, pickle (Give blank if you dont have job.',
+        });
+        if (!jobsInput) {
+            vscode.window.showInformationMessage('Jobs Folder not created.');
+        }
+        let jobNames = [];
+        if (jobsInput) {
+            jobNames = checkSpacesAndReplace(jobsInput.split(',').map(name => name.trim()));
+        }
+        // Folder Generation
         const kubernetesFolder = vscode.Uri.joinPath(vscode.workspace.workspaceFolders[0].uri, 'kubernetes');
         fs.mkdirSync(kubernetesFolder.fsPath, { recursive: true });
-        const namespacesFolder = vscode.Uri.joinPath(kubernetesFolder, 'namespaces');
-        fs.mkdirSync(namespacesFolder.fsPath, { recursive: true });
-        const deploymentsFolder = vscode.Uri.joinPath(kubernetesFolder, 'deployments');
-        fs.mkdirSync(deploymentsFolder.fsPath, { recursive: true });
-        const servicesFolder = vscode.Uri.joinPath(kubernetesFolder, 'services');
-        fs.mkdirSync(servicesFolder.fsPath, { recursive: true });
-        const ingressFolder = vscode.Uri.joinPath(kubernetesFolder, 'ingess');
-        fs.mkdirSync(ingressFolder.fsPath, { recursive: true });
+        for (const deployment of deploymentNames) {
+            const deploymentFolder = vscode.Uri.joinPath(kubernetesFolder, `${deployment}-service`);
+            fs.mkdirSync(deploymentFolder.fsPath, { recursive: true });
+        }
         const configmapsFolder = vscode.Uri.joinPath(kubernetesFolder, 'configmaps');
         fs.mkdirSync(configmapsFolder.fsPath, { recursive: true });
         const secretsFolder = vscode.Uri.joinPath(kubernetesFolder, 'secrets');
         fs.mkdirSync(secretsFolder.fsPath, { recursive: true });
         const jobsFolder = vscode.Uri.joinPath(kubernetesFolder, 'jobs');
         fs.mkdirSync(jobsFolder.fsPath, { recursive: true });
-        const namespaceYAML = "apiVersion: v1\nkind: Namespace\nmetadata:name: ${namespaceName}";
-        fs.writeFileSync(path.join(namespacesFolder.fsPath, 'development.yaml'), namespaceYAML);
-        fs.writeFileSync(path.join(namespacesFolder.fsPath, 'staging.yaml'), namespaceYAML);
-        fs.writeFileSync(path.join(namespacesFolder.fsPath, 'production.yaml'), namespaceYAML);
+        // Deployment Yaml fs operations
+        const namespaceYAML = `
+apiVersion: v1
+kind: Namespace
+metadata:
+  name:
+   ${namespace}`;
+        fs.writeFileSync(path.join(kubernetesFolder.fsPath, 'namespace.yaml'), namespaceYAML);
         const promises = deploymentNames.map((deploymentName) => __awaiter(this, void 0, void 0, function* () {
+            const serviceFolder = vscode.Uri.joinPath(kubernetesFolder, `${deploymentName}-service`);
             const deploymentYAML = `
 apiVersion: apps/v1
 kind: Deployment
 metadata:
-name: ${deploymentName}
-namespace: ${namespaceName}
-# Add other deployment details here
+  name: ${deploymentName}
+  namespace: ${namespace}
+spec:
+  replicas: 3
+  selector:
+    matchLabels:
+      app: my-app
+  template:
+    metadata:
+      labels:
+        app: my-app
+    spec:
+      containers:
+        - name: my-container
+          image: nginx:latest
+          ports:
+            - containerPort: 80
           `;
-            yield writeFileWithDirectoryCheck(path.join(deploymentsFolder.fsPath, `${deploymentName}.yaml`), deploymentYAML);
+            yield writeFileWithDirectoryCheck(path.join(serviceFolder.fsPath, `${deploymentName}-deployment.yaml`), deploymentYAML);
             const serviceYAML = `
 apiVersion: v1
 kind: Service
 metadata:
 name: ${deploymentName}-service
-namespace: ${namespaceName}
+namespace: ${namespace}
 spec:
 selector:
   app: ${deploymentName}
@@ -96,16 +134,16 @@ ports:
     port: 80
     targetPort: 8080
           `;
-            yield writeFileWithDirectoryCheck(path.join(servicesFolder.fsPath, `${deploymentName}-service.yaml`), serviceYAML);
+            yield writeFileWithDirectoryCheck(path.join(serviceFolder.fsPath, `${deploymentName}-service.yaml`), serviceYAML);
             const ingressYAML = `
 apiVersion: networking.k8s.io/v1
 kind: Ingress
 metadata:
 name: ${deploymentName}-ingress
-namespace: ${namespaceName}
+namespace: ${namespace}
 spec:
 rules:
-  - host: ${deploymentName}.example.com
+  - host: ${deploymentName}.com
     http:
       paths:
         - path: /
@@ -116,34 +154,56 @@ rules:
               port:
                 number: 80
           `;
-            yield writeFileWithDirectoryCheck(path.join(ingressFolder.fsPath, `${deploymentName}-ingress.yaml`), ingressYAML);
-            const secretYAML = `
+            yield writeFileWithDirectoryCheck(path.join(serviceFolder.fsPath, `${deploymentName}-ingress.yaml`), ingressYAML);
+        }));
+        const secretYAML = `
 apiVersion: v1
 kind: Secret
 metadata:
-  name: ${deploymentName}-secret
-  namespace: ${namespaceName}
+  name: application-secret
+  namespace: ${namespace}
 type: Opaque
 data:
   username: ${Buffer.from('my-username').toString('base64')}
   password: ${Buffer.from('my-password').toString('base64')}
           `;
-            yield writeFileWithDirectoryCheck(path.join(secretsFolder.fsPath, `${deploymentName}-secret.yaml`), secretYAML);
-            const configMapYAML = `
+        yield writeFileWithDirectoryCheck(path.join(secretsFolder.fsPath, `application-secret.yaml`), secretYAML);
+        const configMapYAML = `
 apiVersion: v1
 kind: ConfigMap
 metadata:
-  name: ${deploymentName}-configmap
-  namespace: ${namespaceName}
+  name: application-configmap
+  namespace: ${namespace}
 data:
   config.properties: |
     key1=value1
     key2=value2
             `;
-            yield writeFileWithDirectoryCheck(path.join(configmapsFolder.fsPath, `${deploymentName}-configmap.yaml`), configMapYAML);
-        }));
+        yield writeFileWithDirectoryCheck(path.join(configmapsFolder.fsPath, `application-configmap.yaml`), configMapYAML);
         yield Promise.all(promises);
         vscode.window.showInformationMessage(`${deploymentNames.length} deployment, service, and ingress files generated successfully.`);
+        // Jobs Yaml fs operations
+        const jobPromises = jobNames.map((jobNames) => __awaiter(this, void 0, void 0, function* () { }));
+        const jobYAML = `
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: ${jobNames}
+spec:
+  completions: 1
+  template:
+    metadata:
+      name: ${jobNames}-pod
+    spec:
+      containers:
+        - name: ${jobNames}-container
+          image: nginx
+      restartPolicy: Never
+  
+        `;
+        yield writeFileWithDirectoryCheck(path.join(jobsFolder.fsPath, `${jobNames}-job.yaml`), jobYAML);
+        yield Promise.all(jobPromises);
+        vscode.window.showInformationMessage(`${jobNames.length} deployment, service, and ingress files generated successfully.`);
     });
 }
 exports.generateKubernetesFiles = generateKubernetesFiles;
